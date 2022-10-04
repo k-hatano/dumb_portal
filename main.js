a1e3a74ca95b50439cb8dc22ed05dc5bcfa934f6
k-hatano <kent.ruffle.mgj626@gmail.com>
2022-10-05 02:59:18 +0900

    Some fix

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ let weatherInfo = {};
 let weathers = [];
 let news = [];
 let whatDays = [];
-let regionCode = 130000;
+let regionCode = undefined;
 
 const getWeatherMapPromise = _ => new Promise((resolve, reject) => {
 	https.get("https://www.jma.go.jp/bosai/weather_map/data/list.json", res => {
@@ -32,20 +32,23 @@ const getWeatherMapPromise = _ => new Promise((resolve, reject) => {
 				// 	});
 				// }).on("error", error2 => {
 				// 	console.error(error2.message);
-				// 	reject();
+				// 	reject(error);
 				// });
 			} catch (error) {
-				console.error(error.message);
-				reject();
+				console.error(error);
+				reject(error);
 			}
 		});
 	}).on("error", error => {
-		console.error(error.message);
-		reject();
+		console.error(error);
+		reject(error);
 	})
 });
 
 const getWeatherPromise = _ => new Promise((resolve, reject) => {
+	if (regionCode == undefined) {
+		regionCode = 130000;
+	}
 	https.get("https://www.jma.go.jp/bosai/forecast/data/forecast/" + regionCode + ".json", res => {
 		let body = "";
 		res.on("data", chunk => body += chunk);
@@ -85,13 +88,15 @@ const getWeatherPromise = _ => new Promise((resolve, reject) => {
 				}
 				resolve();
 			} catch (error) {
-				console.error(error.message);
-				reject();
+				regionCode = undefined;
+
+				console.error(error);
+				reject(error);
 			}
 		});
 	}).on("error", error => {
-		console.error(error.message);
-		reject();
+		console.error(error);
+		reject(error);
 	})
 });
 
@@ -105,13 +110,13 @@ const getWarningPromise = _ => new Promise((resolve, reject) => {
 				warning = json.headlineText;
 				resolve();
 			} catch (error) {
-				console.error(error.message);
-				reject();
+				console.error(error);
+				reject(error);
 			}
 		});
 	}).on("error", error => {
-		console.error(error.message);
-		reject();
+		console.error(error);
+		reject(error);
 	})
 });
 
@@ -122,8 +127,8 @@ const getNewsPromise = _ => new Promise((resolve, reject) => {
 		res.on("end", _ => {
 			xml2js.parseString(body, (error, result) => {
 				if (error) {
-					console.error(error.message);
-					reject();
+					console.error(error);
+					reject(error);
 				} else {
 					news = [];
 					for (let i = 0; i < result['rss']['channel'][0]['item'].length; i++) {
@@ -138,8 +143,8 @@ const getNewsPromise = _ => new Promise((resolve, reject) => {
 			});
 		});
 	}).on("error", error => {
-		console.error(error.message);
-		reject();
+		console.error(error);
+		reject(error);
 	})
 });
 
@@ -176,19 +181,21 @@ const getWhatDayIsTodayPromise = _ => new Promise((resolve, reject) => {
 				console.dir(whatDays);
 				resolve();
 			} catch (error) {
-				console.error(error.message);
-				reject();
+				console.error(error);
+				reject(error);
 			}
 
 			resolve();
 		});
 	}).on("error", error => {
-		console.error(error.message);
-		reject();
+		console.error(error);
+		reject(error);
 	})
 });
 
 const dateLabels = ['日', '月', '火', '水', '木', '金', '土'];
+const stemLabels = ['甲', '乙', '丙', '丁', '戊', '己', '庚', '辛', '壬', '癸'];
+const branchLabels = ['子', '丑', '寅', '卯', '辰', '巳', '午', '未', '申', '酉', '戌', '亥'];
 
 const server = http.createServer((request, response) => {
 	let parsedUrl = url.parse(request.url, true);
@@ -201,6 +208,7 @@ const server = http.createServer((request, response) => {
 			"Content-Type": "text/html"
 		});
 		let today = new Date();
+		let adjustedTime = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
 
 		let content = `
 <head>
@@ -209,8 +217,8 @@ const server = http.createServer((request, response) => {
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimin-scale=0.1, user-scalable=yes">
 <style>td{padding:0;}</style>
 </head>
-<body style="background:#aaa; position: relative; min-height:600px;">
-<div id="content" style="width:800px; height:600px; margin: auto; background:#fff; position: absolute; top:0; bottom:0; left:0; right:0; z-index: 1;">
+<body style="background:#aaa; position: relative; min-height:540px;">
+<div id="content" style="width:720px; height:540px; margin: auto; background:#fff; position: absolute; top:0; bottom:0; left:0; right:0; z-index: 1;">
 <table style="width: 100%; height: 100%; border:0; border-spacing:0; text-shadow:1px 1px 3px #8884;">
 <tr style="background:#111;">
 <th colspan="2" style="color: white; background:#111 linear-gradient(#111, #222); height:60px;">
@@ -219,30 +227,36 @@ const server = http.createServer((request, response) => {
 &nbsp;&nbsp;
 <span id="lunar" style="background: yellow; width:24px; height: 24px; border-radius: 24px; color: black; font-size: small; display: inline-block; text-align: center; vertical-align: middle; line-height: 24px;">
 ${(((((today.getYear() + 1900 - 2009) % 19) * 11 + (today.getMonth() + 1) + today.getDate()) +1) % 30)}
+</span> 
+<span id="stem" style="background: brown; width:24px; height: 24px; color: white; font-size: small; display: inline-block; text-align: center; vertical-align: middle; line-height: 24px;">
+${stemLabels[Math.floor(adjustedTime / 1000 / 60 / 60 / 24 + 7) % 10]}
+</span> 
+<span id="branch" style="background: green; width:24px; height: 24px; color: white; font-size: small; display: inline-block; text-align: center; vertical-align: middle; line-height: 24px;">
+${branchLabels[Math.floor(adjustedTime / 1000 / 60 / 60 / 24 + 5) % 12]}
 </span>
 </div>
 </th>
 </tr>
 <tr>
 <td style="text-align: center; vertical-align: middle;">
-<img style="max-width: 360px; max-height: 560px;" src="${weatherMapUrl}">
+<img style="max-width: 332px; max-height: 332px;" src="${weatherMapUrl}">
 <marquee style="font-size: medium; text-align: left; padding: 2px; color: red;">${warning}</marquee>
 </td>
 <td>
-<table style="width: 440px; height: 420px; border-spacing:0; background: #bbb;">
-${news.map(n => "<tr><td style='background: #bbb linear-gradient(#bbb, #ccc); height:36px; font-size: x-large;'><span style='padding:8px;'>" + n.title + "</span></td></tr>").join('')}
+<table style="width: 360px; height: 360px; border-spacing:0; background: #bbb;">
+${news.map(n => "<tr><td style='background: #bbb linear-gradient(#bbb, #ccc); height:32px; font-size: large;'><span style='padding:2px 8px;'>" + n.title + "</span></td></tr>").join('')}
 </table>
 </td>
 </tr>
 <tr>
 <td colspan="2">
-<table style="font-size: large; width: 800px; height:84px; text-align: left; border:0; background: #eee linear-gradient(#eee, #fff); border-spacing: 0;">
+<table style="font-size: large; width: 720px; height:84px; text-align: left; border:0; background: #eee linear-gradient(#eee, #fff); border-spacing: 0;">
 <tr>
 <th rowspan="3" style="text-align: center; background: #ddd;">
 ${(new Date(weatherInfo.date).getMonth() + 1) + "/" + (new Date(weatherInfo.date).getDate()) + " (" + dateLabels[new Date(weatherInfo.date).getDay()] + ")" + " 発表"}<br />
 ${weatherInfo.region} の天気
 </th>
-${weathers.map(w => "<th style='padding: 2px 8px;'>" + ((new Date(w.date)).getMonth() + 1) + "/" + (new Date(w.date)).getDate() + " (" + dateLabels[(new Date(w.date)).getDay()] + ")" + "</th>" + "<td>" + w.name.replaceAll('　',' ') + "</td>" + "<td>" + w.temperature + "</td></tr><tr>").join('')}
+${weathers.map(w => "<th style='padding: 2px 8px; white-space: nowrap;'>" + ((new Date(w.date)).getMonth() + 1) + "/" + (new Date(w.date)).getDate() + " (" + dateLabels[(new Date(w.date)).getDay()] + ")" + "</th>" + "<td style='white-space: nowrap; text-overflow: ellipsis;'>" + w.name.replaceAll('　',' ') + "</td>" + "<td style='white-space: nowrap;'>" + w.temperature + "</td></tr><tr>").join('')}
 </tr>
 </table>
 </td>
@@ -263,6 +277,11 @@ var dateLabels=['日','月','火','水','木','金','土'];function printTime(){
 
 	    response.end(content);
 	    // console.log(`Sent a response : ${content}`);
+	}).catch(error => {
+		response.writeHead(500, {
+			"Content-Type": "text/html"
+		});
+		response.end(error.message);
 	});
 });
 
